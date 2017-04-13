194affdaab5a45f7d8e0ad9776a3ac5723908eef
“kriscfoster”! <krisfoster@live.ie>
2017-04-13 10:27:44 +0100

    automated poker player

--- AutomatedPokerPlayer.java
+++ AutomatedPokerPlayer.java
@@ -4,7 +4,9 @@ public class AutomatedPokerPlayer extends PokerPlayer {
 
 	public AutomatedPokerPlayer(DeckOfCards d, String playerName, int riskAversion, int bluffLevel) {
 		super(d, playerName);
-		// TODO Auto-generated constructor stub
+		
+		
+		
 	}
 
 }
